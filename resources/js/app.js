c30bf3b9e0c12f4993630d01339168da32ac324a
DonMartinWorks <donmartinworks@gmail.com>
2025-01-27 17:52:24 -0300

    [ALPINE JS] - Instalación del paquete

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import Alpine from 'alpinejs'
+
+/**
+ * Alpine Configuration
+ */
+
+window.Alpine = Alpine
+
+Alpine.start()
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +10,17 @@
     <!-- Links -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
     <h1 class="bg-purple-600 text-white text-center p-2 text-lg">Tailwind CSS</h1>
+
+    <div x-data="{ open: false }">
+        <button @click="open = !open">Expand</button>
+
+        <span x-show="open">
+            Content...
+        </span>
+    </div>
 </body>
+
 </html>
