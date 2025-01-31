76aa7772e41c8a80a61752d0bade282d6ebd118c
DonMartinWorks <donmartinworks@gmail.com>
2025-01-31 18:43:09 -0300

    [HEADER] - Sección de Mi Aprendizaje

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -189,7 +189,80 @@
 
     <div class="hover:text-udemy">Instuctor</div>
 
-    <div class="hover:text-udemy">Mi Aprendizaje</div>
+    <div x-data="{ menuMiAprendizaje: false }" class="relative" @click.away="menuMiAprendizaje = false">
+        <div @mouseover="menuMiAprendizaje = true" @click="menuMiAprendizaje = ! menuMiAprendizaje"
+            class="hover:text-udemy hover:cursor-pointer">
+            Mi Aprendizaje
+        </div>
+        <div x-show="menuMiAprendizaje"
+            class="absolute mt-8 right-0 w-[300px] border border-gray-300 divide-y divide-gray-300">
+            <div>
+                <a href="#" class="flex gap-3 p-4">
+                    <img src="{{ asset('/assets/images/header/aprendizaje/curso1.jpg') }}" alt=""
+                        class="h-16 w-16 object-cover">
+                    <div>
+                        <p class="font-semibold leading-tight">
+                            {{ fake()->sentence }}
+                        </p>
+                        <div class="mt-2 h-2 bg-gray-300">
+                            <div class="h-2 bg-udemy w-[10%]"></div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div>
+                <a href="#" class="flex gap-3 p-4">
+                    <img src="{{ asset('/assets/images/header/aprendizaje/curso2.jpg') }}" alt=""
+                        class="h-16 w-16 object-cover">
+                    <div>
+                        <p class="font-semibold leading-tight">
+                            {{ fake()->sentence }}
+                        </p>
+                        <div class="mt-2 h-2 bg-gray-300">
+                            <div class="h-2 bg-udemy"></div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div>
+                <a href="#" class="flex gap-3 p-4">
+                    <img src="{{ asset('/assets/images/header/aprendizaje/curso3.jpg') }}" alt=""
+                        class="h-16 w-16 object-cover">
+                    <div>
+                        <p class="font-semibold leading-tight">
+                            {{ fake()->sentence }}
+                        </p>
+                        <div class="mt-2 h-2 bg-gray-300">
+                            <div class="h-2 bg-udemy w-[5%]"></div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div>
+                <a href="#" class="flex gap-3 p-4">
+                    <img src="{{ asset('/assets/images/header/aprendizaje/curso4.jpg') }}" alt=""
+                        class="h-16 w-16 object-cover">
+                    <div>
+                        <p class="font-semibold leading-tight">
+                            {{ fake()->sentence }}
+                        </p>
+                        <div class="mt-2 h-2 bg-gray-300">
+                            <div class="h-2 bg-udemy w-1/4"></div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="p-4 text-center">
+                <a href="#" class="block p-4 bg-primary hover:bg-primary-light text-white text-lg">
+                    Ir a Mi Aprendizaje
+                </a>
+            </div>
+        </div>
+    </div>
 
     <div>
         <!-- Heart Icon -->
